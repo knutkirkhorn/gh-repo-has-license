4156174d2f33d5fa3f096e6346e32d9e3bf3b48a
Knut Kirkhorn <knutkirk@hotmail.com>
2023-02-23 23:29:23 +0100

    Use `package-user-agent`

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 import defaultBranch from 'default-branch';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import got from 'got';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import * as cheerio from 'cheerio';
+import getPackageUserAgent from 'package-user-agent';
+
+const packageUserAgent = await getPackageUserAgent();
 
 async function fallbackMethod(repoUrl) {
 	try {
-		const response = await got(repoUrl);
+		const response = await got(repoUrl, {headers: packageUserAgent});
 		const $ = cheerio.load(response.body);
 		const licenseUrlPath = $('a > svg.octicon-law').parent().attr('href');
 		const newLicenseUrl = `https://github.com${licenseUrlPath}`;
-		await got(newLicenseUrl);
+		await got(newLicenseUrl, {headers: packageUserAgent});
 		return true;
 	} catch {
 		return false;
@@ -27,7 +29,7 @@ export default async function ghRepoHasLicense(repo) {
 		const branch = await defaultBranch(repo);
 		// Try first to check this url, since it is the most used
 		const licenseUrl = `${repo}/blob/${branch}/LICENSE`;
-		await got(licenseUrl);
+		await got(licenseUrl, {headers: packageUserAgent});
 		return true;
 	} catch (error) {
 		if (error.response.statusCode !== 404) return false;
